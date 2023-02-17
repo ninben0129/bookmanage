93bf86479c0a5b30166fcbace4d4c17a64e68986
ninben0129 <tanaka.masahiro.r6@dc.tohoku.ac.jp>
2023-02-17 16:23:39 +0900

    barcodedecoderの実装

---
+++ bctest.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+def bcdecoder(path):
+    img = cv2.imread(path)
+    img = cv2.resize(img, (300, 400))
+    # cv2.imshow('image', img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    bd = cv2.barcode.BarcodeDetector()
+    retval, decoded_info, decoded_type, points = bd.detectAndDecode(img)
+    ans = 0
+    for i in decoded_info:
+        if int(i) // 10**10 == 978:
+            ans = i
+    return ans
+    # print(decoded_info)
+
+
+print(bcdecoder('IMG_4296.jpg'))
