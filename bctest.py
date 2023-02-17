6413d5003f8976c537138243c4eca69b4b6cac87
ninben0129 <tanaka.masahiro.r6@dc.tohoku.ac.jp>
2023-02-17 17:04:58 +0900

    大枠の完成

--- bctest.py
+++ bctest.py
@@ -3,7 +3,7 @@ import cv2
 
 def bcdecoder(path):
     img = cv2.imread(path)
-    img = cv2.resize(img, (300, 400))
+    # img = cv2.resize(img, (300, 400))
     # cv2.imshow('image', img)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
@@ -17,4 +17,4 @@ def bcdecoder(path):
     # print(decoded_info)
 
 
-print(bcdecoder('IMG_4296.jpg'))
+# print(bcdecoder('IMG_4296.jpg'))
--- bookmanegerinit.py
+++ bookmanegerinit.py
@@ -3,6 +3,7 @@ import requests
 import json
 import openpyxl
 import glob
+import bctest
 
 url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:'
 wb = openpyxl.load_workbook("booklist.xlsx")
@@ -23,15 +24,27 @@ def isbntoinfo(isbn):
     publisheddate = info['publishedDate']
     pages = info['pageCount']
     language = info['language']
-    print(title)
-    print(author)
-    print(publisher)
-    print(language)
+    # print(title)
+    # print(author)
+    # print(publisher)
+    # print(language)
+    return title, author, publisher, language
 
 
 def main():
-    isbn_input = input("input ISBN >>>")
-    isbntoinfo(isbn_input)
+    tmp = 2
+    # isbn_input = input("input ISBN >>>")
+    for path in files:
+        isbn = bctest.bcdecoder(path)
+        title, author, publisher, language = isbntoinfo(isbn)
+        ws.cell(row=tmp, column=2, value=isbn)
+        ws.cell(row=tmp, column=3, value=title)
+        ws.cell(row=tmp, column=4, value=author)
+        ws.cell(row=tmp, column=5, value=publisher)
+        ws.cell(row=tmp, column=6, value=language)
+        tmp += 1
+    wb.save("booklist.xlsx")
+    wb.close()
 
 
 if __name__ == "__main__":
